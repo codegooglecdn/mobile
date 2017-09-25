685b3e4aef172c2454a45bb2fc94772476b7138e
codegooglecdn <bwat.csb01@gmail.com>
2017-09-25 19:26:03 +0700

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,15 +1,15 @@
 if (document.getElementsByName) {
-  var metaArray = document.getElementsByName('Author');
+  var metaArray = document.getElementsByName('author');
   for (var i=0; i<metaArray.length; i++) {
     document.write(metaArray[i].content + 'babi');
   }
 
-  var metaArray = document.getElementsByName('Description');
+  var metaArray = document.getElementsByName('description');
   for (var i=0; i<metaArray.length; i++) {
     document.write(metaArray[i].content + 'sempak');
   }
 
-  var metaArray = document.getElementsByName('Keywords');
+  var metaArray = document.getElementsByName('keywords');
   for (var i=0; i<metaArray.length; i++) {
     document.write(metaArray[i].content + 'tai');
   }
